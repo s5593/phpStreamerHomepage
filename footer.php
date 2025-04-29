aa84d400fd92eb005f1cdb42679cb649f227084f
s5593 <68044337+s5593@users.noreply.github.com>
2025-04-30 03:32:12 +0900

    s

--- footer.php
+++
@@ -1,6 +0,0 @@
-<!-- /html/footer.php -->
-<footer class="site-footer">
-    <div class="container">
-        <p>&copy; 2025 별구름 커뮤니티. All rights reserved.</p>
-    </div>
-</footer>
--- php/auth/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once(__DIR__ . '/../../lib/common.php');
-
-session_unset();
-session_destroy();
-
-header("Location: /index.php");
-exit;
